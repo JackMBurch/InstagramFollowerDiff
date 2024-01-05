9a209cbfc6bc255328890db4ff6de23a52fc3791
JackMBurch <jackb@worthingtonassembly.com>
2024-01-04 23:18:45 -0500

    Add project files.

---
+++ InstagramFollowerDiff/Models/IGListItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace InstagramFollowerDiff.Models
+{
+    public class IGListItem
+    {
+        [JsonPropertyName("href")]
+        public string Link { get; set; } = string.Empty;
+
+        [JsonPropertyName("value")]
+        public string User { get; set; } = string.Empty;
+
+        [JsonPropertyName("timestamp")]
+        public int Timestamp { get; set; }
+    }
+}
---
+++ InstagramFollowerDiff/Models/Relationships.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace InstagramFollowerDiff.Models
+{
+
+    public class FollowingRoot
+    {
+        [JsonPropertyName("relationships_following")]
+        public List<Relationships> RelationshipsFollowing { get; set; } = [];
+    }
+
+    public class Relationships
+    {
+        [JsonPropertyName("title")]
+        public string Title { get; set; } = string.Empty;
+
+        [JsonPropertyName("media_list_data")]
+        public List<object> MediaListData { get; set; } = [];
+
+        [JsonPropertyName("string_list_data")]
+        public List<IGListItem> StringListData { get; set; } = [];
+    }
+}
